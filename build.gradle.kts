0af3c390b2f16cc6662b10487bb3230a4e200539
T14D3 <73843330+T14D3@users.noreply.github.com>
2025-02-17 19:06:02 +0100

    Finally move gradle to Kotlin DSL
    Signed-off-by: T14D3 <73843330+T14D3@users.noreply.github.com>

---
+++ build.gradle.kts
@@ -0,0 +1,105 @@
+import xyz.jpenilla.runtask.task.AbstractRun
+
+plugins {
+    id("java")
+    id("maven-publish")
+    //id("io.papermc.paperweight.userdev") version "1.7.3"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
+}
+
+group = "de.t14d3"
+version = "0.2.0"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "papermc-repo"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        name = "sonatype"
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+    maven {
+        url = uri("https://repo.extendedclip.com/releases/")
+    }
+    maven {
+        name = "EngineHub"
+        url = uri("https://maven.enginehub.org/repo/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.11.6")
+    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.3.10")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.13")
+    implementation(platform("com.intellectualsites.bom:bom-newest:1.52"))
+    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core")
+    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit")
+}
+
+val targetJavaVersion = 21
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+
+    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+        options.release.set(targetJavaVersion)
+    }
+}
+tasks.withType<AbstractRun>().configureEach {
+    javaLauncher = javaToolchains.launcherFor {
+        vendor.set(JvmVendorSpec.JETBRAINS)
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+    jvmArgs("-XX:+AllowEnhancedClassRedefinition")
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+    filesMatching("paper-plugin.yml") {
+        expand(props)
+    }
+}
+
+tasks {
+    runServer {
+        minecraftVersion("1.21.4")
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/t14d3/zones")
+            credentials {
+                username = project.findProperty("gpr.user").toString() ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key").toString() ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("gpr") {
+            artifactId = project.name.lowercase()
+            groupId = group.toString().lowercase()
+            version = version.toString()
+            from(components["java"])
+        }
+    }
+}
+
